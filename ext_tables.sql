d77d8fda48836cb8982c2f666c52cff66cd8f2f7
Lukas Domnick <lukas.domnick@antwerpes.de>
2014-03-20 15:36:57 +0100

    fixed a sql bug

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,4 @@
 CREATE TABLE fe_users (
-	tx_apdocchecklogin_prof int(11) DEFAULT '',
-	tx_apdocchecklogin_disc int(11) DEFAULT ''
+	tx_apdocchecklogin_prof int(11) DEFAULT NULL,
+	tx_apdocchecklogin_disc int(11) DEFAULT NULL
 );
