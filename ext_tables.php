24c991f1f55f7c3ba48d33a7d9568ecfbf79b6b1
Tobias Pinnekamp <tobias.pinnekamp@eshaker.de>
2016-05-11 13:37:18 +0200

    [TASK] Add static TypoScript file

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Static', 'DocCheck Login');
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	$_EXTKEY,
 	'DocCheckAuthentication',
