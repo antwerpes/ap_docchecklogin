ea3946bf0483fef140ce1832d5ab147b47a67dc7
Thorsten Daners <thorsten.daners@antwerpes.de>
2018-10-01 08:36:27 +0200

    Some code cleanup.

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,46 +10,45 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'DocCheck Login',
-	'description' => 'Official DocCheck Login Extension for Typo3 6.x',
-	'category' => 'plugin',
-	'shy' => false,
-	'version' => '1.1.0',
-	'dependencies' => 'extbase,fluid',
-	'conflicts' => 'tgr_doccheck,kb_md5fepw',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => true,
-	'lockType' => '',
-	'author' => 'antwerpes ag - see README.txt',
-	'author_email' => 'opensource@antwerpes.de',
-	'author_company' => 'antwerpes ag',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => 
-		array (
-			'conflicts' => 
-				array (
-					'kb_md5fepw' => '',
-					'tgr_doccheck' => '',
-				),
-			'depends' => 
-				array (
-					'php' => '5.3.0-0.0.0',
-					'typo3' => '6.0.0-7.6.99',
-					'extbase' => '0.0.0-0.0.0',
-					'fluid' => '0.0.0-0.0.0',
-				),
-			'suggests' => 
-				array (
-				),
-		),
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'DocCheck Login',
+    'description' => 'Official DocCheck Login Extension for Typo3 6.x',
+    'category' => 'plugin',
+    'shy' => false,
+    'version' => '1.1.0',
+    'dependencies' => 'extbase,fluid',
+    'conflicts' => 'tgr_doccheck,kb_md5fepw',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'beta',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => true,
+    'lockType' => '',
+    'author' => 'antwerpes ag - see README.txt',
+    'author_email' => 'opensource@antwerpes.de',
+    'author_company' => 'antwerpes ag',
+    'CGLcompliance' => null,
+    'CGLcompliance_note' => null,
+    'constraints' =>
+        array(
+            'conflicts' =>
+                array(
+                    'kb_md5fepw' => '',
+                    'tgr_doccheck' => '',
+                ),
+            'depends' =>
+                array(
+                    'php' => '5.3.0-0.0.0',
+                    'typo3' => '6.0.0-7.6.99',
+                    'extbase' => '0.0.0-0.0.0',
+                    'fluid' => '0.0.0-0.0.0',
+                ),
+            'suggests' =>
+                array(),
+        ),
 );
 
 ?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,29 +1,31 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Antwerpes.'.$_EXTKEY,
-	'DocCheckAuthentication',
-	array(
-		'DocCheckAuthentication' => 'main, loggedIn, loginForm',
-	),
-	// non-cacheable actions
-	array(
-		'DocCheckAuthentication' => 'main, loggedIn',
-	)
+    'Antwerpes.' . $_EXTKEY,
+    'DocCheckAuthentication',
+    array(
+        'DocCheckAuthentication' => 'main, loggedIn, loginForm',
+    ),
+    // non-cacheable actions
+    array(
+        'DocCheckAuthentication' => 'main, loggedIn',
+    )
 );
 
 // Add the service
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'Antwerpes\\ApDocchecklogin\\DocCheckAuthenticationService', array(
-	'title' => 'DocCheck Authentication Service',
-	'description' => 'Authenticates users through the DocCheck Authentication Service',
-	'subtype' => 'getUserFE,authUserFE',
-	'available' => TRUE,
-	'priority' => 60,
-	'quality' => 60,
-	'os' => '',
-	'exec' => '',
-	'className' => 'Antwerpes\\ApDocchecklogin\\DocCheckAuthenticationService'
+    'title' => 'DocCheck Authentication Service',
+    'description' => 'Authenticates users through the DocCheck Authentication Service',
+    'subtype' => 'getUserFE,authUserFE',
+    'available' => true,
+    'priority' => 60,
+    'quality' => 60,
+    'os' => '',
+    'exec' => '',
+    'className' => 'Antwerpes\\ApDocchecklogin\\DocCheckAuthenticationService'
 ));
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['initFEuser'][] = 'EXT:ap_docchecklogin/Classes/DocCheckAuthenticationService.php:&Antwerpes\ApDocchecklogin\DocCheckAuthenticationService->bypassLoginForCrawling';
